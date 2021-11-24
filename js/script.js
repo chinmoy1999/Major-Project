ffd23dd37fb7447f461bd8f42c1db578d2caebca
chinmoy1999 <67042871+chinmoy1999@users.noreply.github.com>
2021-11-24 16:56:56 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -317,7 +317,7 @@ function insertItemPrice(html,
     return insertProperty(html, pricePropName, "");
   }
 
-  priceValue = "$" + priceValue.toFixed(2);
+  priceValue = "Rs" + priceValue.toFixed(2);
   html = insertProperty(html, pricePropName, priceValue);
   return html;
 }
